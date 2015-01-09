2feafb0ee880910afb4f9796b5fccd96d2e1e6cf
Jin Hu <jin.hu2013@gmail.com>
2015-01-09 22:43:07 +0800

    Update Clone Graph.cpp

--- Medium/Clone Graph.cpp
+++ Medium/Clone Graph.cpp
@@ -8,26 +8,33 @@
  */
  
  /*dfs*/
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
 class Solution {
 public:
 	UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
 		if (node == nullptr) {
-			return nullptr;
-		}
-		return dfs(node);
-	}
-private:
-	unordered_map<int, UndirectedGraphNode*> visit;
-
-	UndirectedGraphNode* dfs(UndirectedGraphNode *node) {
+	        return nullptr;
+	    }
 		if (visit.count(node->label) > 0) {
 			return visit[node->label];
 		}
+		
 		UndirectedGraphNode *newNode = new UndirectedGraphNode(node->label);
 		visit[node->label] = newNode;
+		
 		for (int i = 0; i < node->neighbors.size(); i++) {
-			newNode->neighbors.push_back( dfs(node->neighbors[i]));
+			newNode->neighbors.push_back(cloneGraph(node->neighbors[i]));
 		}
+        
         return newNode;
 	}
+private:
+	unordered_map<int, UndirectedGraphNode*> visit;
 };
