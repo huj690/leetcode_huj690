d4546edafd3faab7b43678dcfff68a679f18f333
Jin Hu <jin.hu2013@gmail.com>
2015-01-27 20:15:47 +0800

    Create Subsets.cpp

---
+++ Backtracking/Subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        if (S.empty()) {
+            return res;
+        }
+        this->S = S;
+        vector<int> sub;
+        enumSubSets(sub, 0);
+        return res;
+    }
+private:
+    vector<int> S;
+    vector<vector<int> > res;
+    void enumSubSets(vector<int> sub, int level) {
+        if (level == S.size()) {
+            res.push_back(sub);
+            return;
+        }
+        sub.push_back(S[level]);
+        enumSubSets(sub, level + 1);
+        sub.pop_back();
+		enumSubSets(sub, level + 1);
+    }
+};
