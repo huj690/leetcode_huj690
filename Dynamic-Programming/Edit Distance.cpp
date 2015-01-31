ae53b81db915baf658e09c40259ba9fb26bb4c5e
Jin Hu <jin.hu2013@gmail.com>
2015-01-31 19:00:33 +0800

    Create Edit Distance.cpp

---
+++ Dynamic-Programming/Edit Distance.cpp
@@ -0,0 +1,39 @@
+/*
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+*/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<int> dp_prev(word1.length() + 1, 0);
+        vector<int> dp = dp_prev;
+        
+        //init
+        for (int j = 0; j <= word1.length(); j++) {
+            dp_prev[j] = j;
+        }
+        
+        //build dp
+        int i_index, j_index;
+        for (int i = 1; i <= word2.length(); i++) {
+            dp[0] = i;
+            for (int j = 1; j <= word1.length(); j++) {
+                i_index = i - 1;
+                j_index = j - 1;
+                dp[j] = min (dp_prev[j], 
+                        min(dp[j - 1], 
+                        word2[i_index] == word1[j_index] ? dp_prev[j - 1] - 1 : dp_prev[j - 1]
+                        )) + 1;
+            }
+            dp_prev = dp;
+            dp.assign(word1.length() + 1, 0);
+        }
+        return dp_prev[word1.length()];
+     }
+};
