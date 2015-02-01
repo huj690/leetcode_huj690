26e18d5786954e3d8163a9c46b4cd00d6e9b7389
Jin Hu <jin.hu2013@gmail.com>
2015-02-01 11:08:36 +0800

    Update Edit Distance.cpp

--- Dynamic-Programming/Edit Distance.cpp
+++ Dynamic-Programming/Edit Distance.cpp
@@ -37,3 +37,34 @@ public:
         return dp_prev[word1.length()];
      }
 };
+
+
+//滚动数组
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        if (word2.length() < word1.length()) {
+            return minDistance(word2, word1);
+        }
+        vector<int> dp(word1.length() + 1, 0);
+        int upper_left = 0; // dp[i - 1][j - 1]
+        
+        // init
+        for (int j = 0; j <= word1.length(); j++) {
+            dp[j] = j;
+        }
+        
+        // build dp
+        for (int i = 1; i <= word2.length(); i++) {
+            upper_left = dp[0];
+            dp[0] = i;
+            
+            for (int j = 1; j <= word1.length(); j++) {
+                int upper = dp[j];
+                dp[j] = min (dp[j], min(dp[j - 1], word2[i - 1] == word1[j - 1] ? upper_left - 1 : upper_left)) + 1;
+                upper_left = upper;
+            }
+        }
+        return dp[word1.length()];
+     }
+};
