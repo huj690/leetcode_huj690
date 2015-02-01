0faad09dc9bc461f240546984ec20eea2c5c0baa
Jin Hu <jin.hu2013@gmail.com>
2015-02-01 23:49:15 +0800

    Create Merge Two Sorted Lists.cpp

---
+++ Linked-List/Merge Two Sorted Lists.cpp
@@ -0,0 +1,24 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        
+        ListNode* dummy = new ListNode(-1), *p = dummy;
+        for (;  l1 != NULL && l2 != NULL; p = p->next) { // 这个for循环的用法很好，注意终止条件是&& 还是||
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+        }
+        
+        p->next = l1 != NULL ? l1 : l2; // 别忘了
+        return dummy->next;
+    }
+};
