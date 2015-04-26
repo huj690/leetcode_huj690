b60e4407a2d19b8360f2cc879b3a5919831f69b2
Jin Hu <jin.hu2013@gmail.com>
2015-04-26 17:40:55 +0800

    Update String to Integer.cpp

--- String/String to Integer.cpp
+++ String/String to Integer.cpp
@@ -16,49 +16,36 @@ test case:
 123213.1231231
 */
 
-
 class Solution {
 public:
-    int atoi(const char *str) {
-        if (strlen(str) == 0) {
+    int myAtoi(string str) {
+        if (str.empty()) {
             return 0;
         }
         
-        int i = 0;
-        while (str[i] == ' ') {
+        int sig = 1;
+        int i = 0, len = str.length();
+        while (i < len && str[i] == ' ') {
             i++;
         }
-        bool positive = true;
-        if (str[i] == '-') {
-            positive = false;
+        if (str[i] == '+') {
             i++;
-        } else if (str[i] == '+') {
+        } else if (str[i] == '-'){
+            sig = -1;
             i++;
-        }
-        
-        string valid = "";
-        while(str[i] <= '9' && str[i] >= '0') {
-            valid += str[i];
-            i++;
-        }
-        
-		if (valid.length() > 10) {
-				return positive == true ? INT_MAX : INT_MIN;
-			}
-        if (valid.length() == 10) {
-            if (positive && valid > "2147483647") {
-                return INT_MAX;
-            }
-            if (!positive && valid > "2147483648"){
-                return INT_MIN;
-            }
         }
         
         int res = 0;
-        while (!valid.empty()) {
-            res = res * 10 + valid[0] - '0';
-            valid = valid.substr(1, valid.length() - 1);
+        for (; i < len; i++) {
+            if (str[i] > '9' || str[i] < '0') {
+                break;
+            }
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && str[i] - '0' > INT_MAX % 10)) {
+                return sig < 0 ? INT_MIN : INT_MAX;
+            }
+            res = res * 10 + str[i] - '0';
         }
-        return positive == true ? res : (-1) * res;
+        return sig * res;
+        
     }
 };
