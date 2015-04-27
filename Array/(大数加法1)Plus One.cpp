3147f09496abb5edccedd9b9ac7d50d6d06c95a0
Jin Hu <jin.hu2013@gmail.com>
2015-04-27 23:31:18 +0800

    Update (大数加法1)Plus One.cpp

--- Array/(大数加法1)Plus One.cpp
+++ Array/(大数加法1)Plus One.cpp
@@ -2,6 +2,31 @@
 Given a non-negative number represented as an array of digits, plus one to the number.
 The digits are stored such that the most significant digit is at the head of the list.
 */
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if (digits.empty()) {
+            return digits;
+        }
+        add(digits, 1);
+        return digits;
+    }
+private:
+    void add(vector<int> &digits, int carry) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int val = (carry + digits[i]) % 10;
+            carry = (carry + digits[i]) / 10;
+            digits[i] = val;
+        }
+        if (carry > 0) {
+            digits.insert(digits.begin(), 1);
+        }
+    }
+};
+
+
+
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
